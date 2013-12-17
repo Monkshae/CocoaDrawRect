3634dbf42f17d1f08a534556d38a28580dc21def
Mabye <licong@ricongdeMac-mini.local>
2013-12-17 15:44:46 +0800

    Initial Commit

---
+++ CocoaDrawRect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaDrawRect
+//
+//  Created by Mabye on 13-12-17.
+//  Copyright (c) 2013年 Maybe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
